c0e87037e3973828ef2d9f86d6497b26de525be7
KyrianU <kyrian.uwaegbulem@gmail.com>
2024-05-30 04:38:08 +0000

    added basic javascript functionality to the leaderboard screen

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -1,2 +1,7 @@
 const leaderboardList = document.querySelector('#leaderboardList')
-const 
+const highscores = JSON.parse(localStorage.getItem('highscores'))
+
+leaderboardList.innerHTML = 
+highscores.map(score => {
+    return `li class=li class="high-score>${score.name} - ${score.score}</li>`
+}).join('')
